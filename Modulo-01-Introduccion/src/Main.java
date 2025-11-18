5cf61180e64b8f54e58072e5c313b322eaf4ccab
Jonathan Pacheco <JonathanPachecoCruz@hotmail.com>
2025-11-17 23:14:26 -0600

    feat: add Modulo-01-Introduccion

---
+++ Modulo-01-Introduccion/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    String nombre = "Jonathan Pacheco Cruz";
+    System.out.println("Hello, World!");
+    System.out.println("mi nombre es = " + nombre);
+  }
+}
