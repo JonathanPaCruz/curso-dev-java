4e358006556957f6a97500219ffd1ebbc72f2e74
Jonathan Pacheco <JonathanPachecoCruz@hotmail.com>
2025-11-17 23:54:16 -0600

    feat: add Modulo-02-variables-primitivas

---
+++ Modulo-02-variables-primitivas/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello and welcome!");
+    short testShrt = 20;
+    System.out.println("testShrt = " + testShrt);
+    long testLong = 23;
+    System.out.println("testLong = " + testLong);
+    byte age =  26;
+    System.out.println("age = " + age);
+    age = 30;
+    System.out.println("age = " + age);
+    float price = 20.5f;
+    System.out.println("price = " + price);
+    double total = 26.99;
+    System.out.println("total = " + total);
+    int xAxis, yAxis;
+    xAxis = 10;
+    System.out.println("xAxis = " + xAxis);
+    yAxis = 20;
+    System.out.println("yAxis = " + yAxis);
+    char initial = 'j';
+    System.out.println("initial = " + initial);
+    boolean isTrue = true;
+    System.out.println("isTrue = " + isTrue);
+    boolean isFalse = false;
+    System.out.println("isFalse = " + isFalse);
+  }
+}
