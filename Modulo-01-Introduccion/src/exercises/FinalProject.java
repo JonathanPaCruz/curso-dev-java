f82c3bad76cd5ea110c3d93c14a2e390044b3642
Jonathan Pacheco <JonathanPachecoCruz@hotmail.com>
2025-11-27 00:21:38 -0600

    refac: refactor constant string message and others messages

--- Modulo-01-Introduccion/src/exercises/FinalProject.java
+++ Modulo-01-Introduccion/src/exercises/FinalProject.java
@@ -26,38 +26,38 @@ public class FinalProject {
 
 
     //Mensajes en pantalla
-    String welcome = "Bienvenido al sistema de registro de calificaciones!";
-    String firstIndication = "Favor de ingresar la edad del estudiante: ";
-    String secondIndication = "A continuación se le pedirá registrar las 3 calificaciones del estudiante: ";
-    String ageMessage = "La edad del estudiante es: ";
-    String approvedMessage = "Felicidades estás aprobado.";
-    String failedMessage = "Lo siento estás reprobado.";
-    String averageMessage = "Tu promedio es de: ";
-    String optionMessage = "¿Deseas ingresar nuevos registros? \n - [1] para sí. \n - [0] para terminar ejecución.";
+    final String WELCOME = "Bienvenido al sistema de registro de calificaciones!";
+    final String FIRST_INDICATION = "Favor de ingresar la edad del estudiante: ";
+    final String SECOND_INDICATION = "A continuación se le pedirá registrar las 3 calificaciones del estudiante: ";
+    final String AGE_MESSAGE = "La edad del estudiante es: ";
+    final String APPROVED_MESSAGE = "Felicidades el alumno ha aprobado.";
+    final String FAILED_MESSAGE = "Lo siento el alumno ha reprobado.";
+    final String AVERAGE_MESSAGE = "El promedio es de: ";
+    final String OPTION_MESSAGE = "¿Desea ingresar nuevos registros? \n - [1] para sí. \n - [0] para terminar ejecución.";
 
     //Código
     do {
-      System.out.println(welcome);
-      System.out.println(firstIndication);
+      System.out.println(WELCOME);
+      System.out.println(FIRST_INDICATION);
       age = userInput.nextInt();
-      System.out.println(ageMessage + age);
-      System.out.println(secondIndication);
-      System.out.println("Ingresa la 1er calificación:");
+      System.out.println(AGE_MESSAGE + age);
+      System.out.println(SECOND_INDICATION);
+      System.out.println("Ingrese la 1er calificación:");
       firstScore = userInput.nextDouble();
-      System.out.println("Ingresa la 2da calificación:");
+      System.out.println("Ingrese la 2da calificación:");
       secondScore = userInput.nextDouble();
-      System.out.println("Ingresa la 3er calificación:");
+      System.out.println("Ingrese la 3er calificación:");
       thirdScore = userInput.nextDouble();
 
       addition = (firstScore + secondScore + thirdScore) / 3;
-      System.out.println(averageMessage + addition);
+      System.out.println(AVERAGE_MESSAGE + addition);
 
       if (addition >= average) {
-        System.out.println(approvedMessage);
+        System.out.println(APPROVED_MESSAGE);
       } else
-        System.out.println(failedMessage);
+        System.out.println(FAILED_MESSAGE);
 
-      System.out.println(optionMessage);
+      System.out.println(OPTION_MESSAGE);
       option = userInput.nextInt();
       repeat = option == 1;
 
