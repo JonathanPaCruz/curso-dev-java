809321a757b2688d15498594a3824074324bbbc7
Jonathan Pacheco <JonathanPachecoCruz@hotmail.com>
2025-11-26 23:04:16 -0600

    refac: refactor ScanerExample.java code

--- Modulo-05-scanner/src/inputKeyboard/ScannerExample.java
+++ Modulo-05-scanner/src/inputKeyboard/ScannerExample.java
@@ -4,22 +4,30 @@ import java.util.Scanner;
 
 public class ScannerExample {
   public static void main(String[] args) {
-    Scanner textInput = new Scanner(System.in);
+    Scanner userInput = new Scanner(System.in);
     System.out.println("Ingresa el 1er número entero!");
-    int numberCompare1 = textInput.nextInt();
+    int firstNumber = userInput.nextInt();
 
     System.out.println("Ingresa el 2do número entero!");
-    int numberCompare2 = textInput.nextInt();
+    int secondNumber = userInput.nextInt();
 
-    System.out.println("La suma de los números es: " + (numberCompare1 + numberCompare2));
-    System.out.println("La diferencia de los números es: " + (numberCompare1 - numberCompare2));
-    if (numberCompare1 > numberCompare2){
+    int sum = firstNumber + secondNumber;
+    int difference = firstNumber - secondNumber;
+    boolean compare = firstNumber > secondNumber;
+
+    System.out.println("La suma de los números es: " + sum);
+    System.out.println("La diferencia de los números es: " + difference);
+
+    if (compare) {
       System.out.println("El 1er número es mayor que el 2do.");
-    } else if (numberCompare1 == numberCompare2) {
+    } else if (firstNumber == secondNumber) {
       System.out.println("Los números son iguales.");
-    }else {
-      System.out.println("El 2do número es menor que el 1er.");
+    } else {
+      System.out.println("El 2do número es mayor que el 1er.");
     }
+
+    System.out.println("fin del aplicativo");
+    userInput.close();
 
   }
 }
