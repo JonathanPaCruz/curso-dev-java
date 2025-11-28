7cd60557e25203dc91b508e806c1592535b19e5d
Jonathan Pacheco Cruz <JonathanPachecoCruz@hotmail.com>
2025-11-28 01:47:30 -0600

    chore: add new methods

--- all-exercises/src/exercises/StringExercise.java
+++ all-exercises/src/exercises/StringExercise.java
@@ -4,21 +4,28 @@ public class StringExercise {
     public static void main(String[] args) {
 
         //Método length().
-        String welcome = "Hola, Mundo!";
+        String welcome = "Hola, mundo!";
         System.out.println("Longitud de welcome es: " + welcome.length());//12
+
         //Método charAt(int index).
         System.out.println("El carácter en la pos [2] es: " + welcome.charAt(2));//l
+
+        //Método substring(int beginIndex, int endIndex).
+        System.out.println("Subcadena: " + welcome.substring(0, 5)); //Hola,
+
         //Métodos toLowerCase() y toUpperCase().
         System.out.println("Minúsculas: " + welcome.toLowerCase()); //hola, mundo!
         System.out.println("Mayúsculas: " + welcome.toUpperCase()); //HOLA, MUNDO!
 
-        //Método equals(Objet obj)
+        //Método equals(Objet obj).
         String text1 = "hola";
         String text2 = "Hola";
         System.out.println("¿Son iguales?: " + text1.equals(text2)); //false
 
-        //Método equalsIgnoreCase(String str)
+        //Método equalsIgnoreCase(String str).
         System.out.println("¿Son iguales? (ignorando mayúsculas de minúsculas): " + text1.equalsIgnoreCase(text2)); //true
 
+        //Método contains(CharSequence s).
+        System.out.println("¿El mensaje de Hola, Mundo!, contiene Mundo?: " + welcome.contains("mundo")); //true
     }
 }
